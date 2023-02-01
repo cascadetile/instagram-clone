46b468604df91ec0227a3d41344d7637419ce9c3
sch-aa-vk <sch.aa.vk@gmail.com>
2023-02-01 21:50:07 +0600

    feat: add back button to menu

--- src/components/SettingsMenu/index.tsx
+++ src/components/SettingsMenu/index.tsx
@@ -5,13 +5,14 @@ export interface ISettingsMenu {
   setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const SettingsMenu: React.FC<ISettingsMenu> = () => {
+export const SettingsMenu: React.FC<ISettingsMenu> = ({ setOpenMenu }) => {
   return (
     <menu className="settings__menu">
       <ul className="settings__menu-list">
         <li className="settings__menu-item">Switch Appearence</li>
         <li className="settings__menu-item">Switch Language</li>
         <li className="settings__menu-item">Log Out</li>
+        <li className="settings__menu-item" onClick={() => setOpenMenu(false)}>Back</li>
       </ul>
     </menu>
   );
